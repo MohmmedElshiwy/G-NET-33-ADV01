b5ceb4fe2542a70435d72fa27b4a2a22025bee23
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-03-29 10:17:05 +0300

    Problem 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
 ﻿namespace G_NET_33_ADV01;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //Q1: What is a generic class? Why use generics?
+
+        /*
+         A) Generic Class is a class That Can work with any Data Type Without THe Need TO Create Multiple Classes For Each Data Type
+
+        B) We Use Genereic Classes TO Avoid Code Duplication, Increase Code Reusability, And Provide Type Safety At Compile Time
+        */
+
+       
+   
+    
+   
+   
+   
+   
+   
     }
 }
